c8747f799f14512ad151b515c2ca9ddeba7184a5
PauloGerson <paulogerson81@gmail.com>
2021-09-27 10:30:07 -0300

    update

--- Exercicio/cadastro/script.js
+++ Exercicio/cadastro/script.js
@@ -13,24 +13,28 @@ function meuEscopo() {
   //outra maneira de tirar o comportamento padrão do submit
 
   function recebeEventoForm(evento) {
-    evento.preventDefault();
+    evento.preventDefault(); // Tirar o comportamento padrão do submit
     const nome = form.querySelector(".nome1");
     const sobreNome = form.querySelector(".sobreNome");
     const peso = form.querySelector(".peso");
     const altura = form.querySelector(".altura");
 
-    /* pessoas.push({
-        nome,
-        sobreNome,
-        peso,
-        altura
-    }); */
     console.log(this)
+   /*  var paragrafo = document.createElement("p");
+    resultado.appendChild(paragrafo)
+    var conteudo = document.createTextNode(` ${pessoas[0].nome.value} 
+    ${pessoas[0].sobreNome.value} ${pessoas[0].peso.value} ${pessoas[0].altura.value}`);
+    paragrafo.appendChild(conteudo) */
 
-    resultado.innerHTML += `<p> ${nome.value} ${sobreNome.value} ${peso.value} ${altura.value}</p>`;
-  }
+    var paragrafo = document.createElement("p")
+    var conteudo = document.createTextNode(`${nome.value}`)
+    paragrafo.appendChild(conteudo);
+    resultado.appendChild(paragrafo);
+    
+
+   
+}
   form.addEventListener("submit", recebeEventoForm);
 
 }
 
-meuEscopo();
