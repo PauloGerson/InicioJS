af13a5bb0023e6e12dcd2a7e9f82954b4e13a146
PauloGerson <paulogerson81@gmail.com>
2021-10-27 17:39:31 -0300

    outra versão do js

--- Site-faq/script.js
+++ Site-faq/script.js
@@ -1,26 +1,27 @@
 let para = document.querySelector(".contM");
 let para1 = document.querySelector(".contm");
-let botao = document.querySelector('.btn');
-let botao1 = document.querySelector('.btn1')
+let botao = document.querySelectorAll('.btn');
+
 
 let contador = 0;
 let contador1 = 0;
 para.innerHTML = contador;
 
-botao.addEventListener('click',function(){
-  contador++;
-  
-  para.innerHTML = contador;
-  contador1 = contador;
-
- 
-});
-botao1.addEventListener('click',function(){
-  contador = contador1 -1;
-  if(contador1 <= 0){
-    contador1 = 1;
-  }
-    contador1--;
-    para.innerHTML = contador1;
+[...botao].forEach(btn =>{
+  btn.addEventListener('click',function(){
+    choice(this.innerHTML)
   })
+})
 
+function choice(value){
+  switch(value){
+    case "+":
+
+    break;
+
+    case "-":
+
+    break;
+
+  }
+}
